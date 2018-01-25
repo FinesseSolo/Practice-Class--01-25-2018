c17008366d6ca1284ffbc21642f94ddde30d4545
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-01-25 14:41:23 -0500

    first

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+int age ;
+double gpa ;
+int JCSUID;
+	public Student(int age1, double gpa2, int JCSUID3 ) {
+		// TODO Auto-generated constructor stub
+		age = age1;
+		gpa = gpa2;
+		JCSUID = JCSUID3;
+	}
+	
+
+}
---
+++ StudentTest.java
@@ -0,0 +1,10 @@
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student student = new Student (18, 4.0, 245460);
+System.out.println (student.age + "\t" + student.gpa +"\t" + student.JCSUID);
+	}
+
+}
