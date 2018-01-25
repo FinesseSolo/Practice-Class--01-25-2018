5b2e9c674836d30e12a08290a6012482c6443543
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-01-25 14:10:21 -0500

    first

---
+++ Actor.java
@@ -0,0 +1,14 @@
+
+public class Actor {
+	String firstname ;
+	String lastname ;
+	public Actor(String first, String last) {
+	firstname = first;
+	 lastname= last;
+	
+	
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ ActotTest.java
@@ -0,0 +1,10 @@
+
+public class ActotTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Actor actor = new Actor ("Arnold", "Schwarzenegger");
+		System.out.println (actor.firstname);
+	}
+
+}
